8bdb78dc068565e869a735848657b728fb12bb56
Ianadika44 <adika19ian@gmail.com>
2020-04-29 19:28:33 +0300

    removed the docstrings and unnecessary links in articles test

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
 import os
 class Config:
-    '''
-    General configuration parent class
-    '''
+    
     NEWS_API_BASE_URL ='http://newsapi.org/v2/{}?apiKey={}&{}'
-    #   http://newsapi.org/v2/sources?apiKey=asdasdad&
-    #   http://newsapi.org/v2/top-headlines?apiKey=asdasdad&sources=bbc-news
-    #   http://newsapi.org/v2/top-headlines?apiKey=asdasdad&category=business
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
