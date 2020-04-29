6856062cefa3279e43d87b421fa69d83d53b3b20
Ianadika44 <adika19ian@gmail.com>
2020-04-29 19:23:51 +0300

    edited the articles test by removing the docstrings

--- tests/article_test.py
+++ tests/article_test.py
@@ -4,14 +4,9 @@ Article = article.Article
 
 
 class ArticleTest(unittest.TestCase):
-    '''
-    Test Class to test the behaviour of the Movie class
-    '''
-
+    
     def setUp(self):
-        '''
-        Set up method that will run before every Test
-        '''
+        
 
         self.new_article = Article('','','','','','','','')
 
@@ -19,13 +14,9 @@ class ArticleTest(unittest.TestCase):
         self.assertTrue(isinstance(self.new_article,Article))
         
 class SourceTest(unittest.TestCase):
-    """
-    Test Class to test behaviour of source class
-    """
+    
     def setUp(self):
-        '''
-        Set up method that will run before every Test
-        '''
+        
         self.new_source = Source('','','','','','')
         
     def test_instance(self):
